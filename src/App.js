6d0c7a002a1b77625556351ef25deea5bc154e29
Ahmed <ahmed.belhajhasan@esprit.tn>
2021-09-02 01:49:35 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PersonList from './Components/PersonList';
+function App() {
+  return (
+    <div className="App">
+      <h1>  List</h1>
+     <PersonList/>
+    </div>
+  );
+}
+
+export default App;
